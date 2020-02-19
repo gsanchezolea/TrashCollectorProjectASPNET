7cfd041248b7641cc2a411a0116fb5ead5d48967
unknown <thetavog@gmail.com>
2020-02-19 09:21:07 -0600

    added the roles to the register view, migrated, added db sets to generate tables in the server

--- TrashCollectorProject/Data/ApplicationDbContext.cs
+++ TrashCollectorProject/Data/ApplicationDbContext.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TrashCollectorProject.Models;
 
 namespace TrashCollectorProject.Data
 {
@@ -11,6 +13,22 @@ namespace TrashCollectorProject.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Name = "Admin",
+                NormalizedName = "ADMIN"
+            }
+            );
+
+            DbSet<Employee> Employees;
+            DbSet<Customer> Customers;
+            DbSet<Account> Accounts;
+            DbSet<Address> Addresses;
+            DbSet<History> Histories;
         }
     }
 }
--- TrashCollectorProject/Startup.cs
+++ TrashCollectorProject/Startup.cs
@@ -30,8 +30,9 @@ namespace TrashCollectorProject
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultUI().AddDefaultTokenProviders();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
